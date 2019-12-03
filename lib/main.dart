1c9a57173c182088d92fd76d6f21cefddb0f2024
Sean Hellum <seanhellum45@gmail.com>
2019-12-03 02:57:56 +0000

    update

--- lib/main.dart
+++ lib/main.dart
@@ -5,14 +5,29 @@ void main() => runApp(MainActivity());
 class MainActivity extends StatelessWidget {
     @override
     Widget build(BuildContext context) {
+        final _biggerFont = const TextStyle(fontSize: 18.0);
         return MaterialApp(
             title: "FlutterList",
             home: Scaffold(
                 appBar: AppBar(
-                    title: Text("FlutterList"),
+                    title: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Center(
+                            child: Text(
+                                "FlutterList"
+                            )
+                        ),
+                    ),
                 ),
                 body: Center(
-                    child: Text("WIP"),
+                    child: new ListTile(
+                        title: Center(
+                            child: Text(
+                                "WIP",
+                                style: _biggerFont,
+                            ),
+                        )
+                    )
                 ),
             ),
         );
