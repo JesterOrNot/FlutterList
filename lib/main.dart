e6fa64517accbe11372067183bf9719719ca8a11
Sean Hellum <seanhellum45@gmail.com>
2019-12-03 03:03:36 +0000

    update app

--- lib/main.dart
+++ lib/main.dart
@@ -3,33 +3,27 @@ import 'package:flutter/material.dart';
 void main() => runApp(MainActivity());
 
 class MainActivity extends StatelessWidget {
-    @override
-    Widget build(BuildContext context) {
-        final _biggerFont = const TextStyle(fontSize: 18.0);
-        return MaterialApp(
-            title: "FlutterList",
-            home: Scaffold(
-                appBar: AppBar(
-                    title: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Center(
-                            child: Text(
-                                "FlutterList"
-                            )
-                        ),
-                    ),
-                ),
-                body: Center(
-                    child: new ListTile(
-                        title: Center(
-                            child: Text(
-                                "WIP",
-                                style: _biggerFont,
-                            ),
-                        )
-                    )
-                ),
-            ),
-        );
-    }
+  @override
+  Widget build(BuildContext context) {
+    final _biggerFont = const TextStyle(fontSize: 18.0);
+    return MaterialApp(
+      title: "FlutterList",
+      home: Scaffold(
+        appBar: AppBar(
+          title: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Center(child: Text("FlutterList")),
+          ),
+        ),
+        body: Center(
+            child: new ListTile(
+                title: Center(
+          child: Text(
+            "WIP",
+            style: _biggerFont,
+          ),
+        ))),
+      ),
+    );
+  }
 }
